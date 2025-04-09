c3613d583834a17b5acc02273ddfd8cfb002d45f
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-04-09 14:29:16 -0300

    feat: masks.ts to add field formatting, such as cpf in xxx.xxx.xxx-xx format

---
+++ utils/masks.ts
@@ -0,0 +1,15 @@
+export const cpfMask = (value: string) => {
+  return value
+    .replace(/\D/g, '')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+    .replace(/(-\d{2})\d+?$/, '$1')
+}
+
+export const phoneMask = (value: string) => {
+  return value
+    .replace(/\D/g, '')
+    .replace(/^(\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{5})(\d{4})$/, '$1-$2')
+}
